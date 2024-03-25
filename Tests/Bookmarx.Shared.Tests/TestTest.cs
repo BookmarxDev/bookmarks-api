1df6d2f933771f5689b8cddf3a884d2c9c70f5de
Charles J <143773361+Hyper-Text-Coffee-Pot@users.noreply.github.com>
2024-03-24 23:31:53 -0500

    Push to test testing runs

---
+++ Tests/Bookmarx.Shared.Tests/TestTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Bookmarx.Shared.Tests;
+
+public class TestTest : IDisposable
+{
+	public TestTest()
+	{
+		// Do some setup
+	}
+
+	public void Dispose()
+	{
+		// Do some teardown
+	}
+
+	[Fact]
+	public void Succeed()
+	{
+		// Test method implementation
+		Assert.True(1 == 1);
+	}
+
+	[Fact]
+	public void Fail()
+	{
+		// Test method implementation
+		Assert.True(1 == 2);
+	}
+}
